f541a914fe8ad1b9c146f669f8f5ee8336fbb617
KBY\tetep <tetepiti149@gmail.com>
2024-12-27 20:18:23 +0900

    RWLock 매크로 사용

--- MyServer/MultiThreadPractice/RWLockPractice.h
+++ MyServer/MultiThreadPractice/RWLockPractice.h
@@ -8,25 +8,25 @@ using namespace std;
 template<typename T>
 class CustomQueue {
 public:
-	RWLock _locks[1];
+	USE_RWLOCK
 
 public:
 	void push(T val) {
-		WriteLockGuard writeLockGuard_(_locks[0]);
+		WRITE_RWLOCK
 		_Q.push(val);
 	}
 	T pop() {
-		WriteLockGuard writeLockGuard_(_locks[0]);
+		WRITE_RWLOCK
 		T ans = _Q.front();
 		_Q.pop();
 		return ans;
 	}
 	T peek() {
-		ReadLockGuard readLockGuard_(_locks[0]);
+		READ_RWLOCK
 		return _Q.front();
 	}
 	bool empty() {
-		ReadLockGuard readLockGuard_(_locks[0]);
+		READ_RWLOCK
 		return _Q.empty();
 	}
 
