3d2f28692fc6f174aaa78916ece11476e30f44e7
KBY\tetep <tetepiti149@gmail.com>
2024-12-27 19:20:24 +0900

    각 연습 코드들을 별도의 모듈화

--- MyServer/MultiThreadPractice/futurePractice.h
+++ MyServer/MultiThreadPractice/futurePractice.h
@@ -1,45 +1,11 @@
 #pragma once
 #include <thread>
 #include <future>
-#include <vector>
-#include <functional>
 
 using namespace std;
 
-int calculate(int startnum = 0) {
-	int ans = startnum;
-	for (int i = 0; i < 10'000; i++) ans += 1;
-	return ans;
-}
+int calculate(int startnum);
 
-void promiseCal(promise<int>& pr, int startnum = 0) {
-	int ans = startnum;
-	for (int i = 0; i < 10'000; i++) ans += 1;
-	pr.set_value(ans);
-}
+void promiseCal(promise<int>& pr, int startnum);
 
-class futurePractice {
-public:
-	template<typename... Args>
-	void CalByAsync(function<int(Args...)>& func, Args... args) {
-		_fts.push_back(async(launch::async, func, args...));
-	}
-
-	template<typename... Args>
-	void CalByPromise(function<int(Args...)>& func, Args... args) {
-		promise<int> pr;
-		_fts.push_back(pr.get_future());
-		thread thr(, )
-
-	}
-
-	template<typename... Args>
-	void CalByPackagedTask(function<int(Args...)>& func, Args... args) {
-
-	}
-
-private:
-	promise<int> _pr;
-	vector<future<int>> _fts;
-};
-
+void FuturePractice();
