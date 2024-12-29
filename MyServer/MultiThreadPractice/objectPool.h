6f5574d7c7e04802f556ad76f1336ad5de39c83b
KBY\tetep <tetepiti149@gmail.com>
2024-12-29 09:55:19 +0900

    objectPool의 디버그용 멤버 함수 추가

--- MyServer/MultiThreadPractice/objectPool.h
+++ MyServer/MultiThreadPractice/objectPool.h
@@ -2,24 +2,39 @@
 #include <cstdint>
 #include <windows.h>
 #include <malloc.h>
-#include "pch.h"
+#include <atomic>
+
+using namespace std;
 
 struct poolEntry : public SLIST_ENTRY {
 	uint32_t size;
 };
 
+#ifdef _DEBUG
+class counter {
+private:
+	atomic<uint32_t> _uses = 0;
+	atomic<uint32_t> _reserves = 0;
+};
+#endif
+
 template<typename _Ty>
 class objectPool {
 public:
-	static void* alloc() {
-		poolEntry* pTopEntry = static_cast<poolEntry*>(::InterlockedPopEntrySList(&_poolHeader));
-		if (pTopEntry == nullptr) {
-			pTopEntry = static_cast<poolEntry*>(_aligned_malloc(sizeof(poolEntry) + _typeSize, 16));
+	static poolEntry* popEntry() {
+		poolEntry* pEntry = static_cast<poolEntry*>(::InterlockedPopEntrySList(&_poolHeader));
+		if (pEntry == nullptr) {
+			pEntry = static_cast<poolEntry*>(_aligned_malloc(sizeof(poolEntry) + _typeSize, 16));
 		}
+		return pEntry;
 	}
+
 private:
 	alignas(16) static SLIST_HEADER _poolHeader;
 	static uint32_t _typeSize;
+#ifdef _DEBUG
+	static counter;
+#endif
 };
 
 template<typename _Ty>
