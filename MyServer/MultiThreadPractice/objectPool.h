1ba842ce14088f5e65166a1e0d01d459ecbc9216
KBY\tetep <tetepiti149@gmail.com>
2024-12-29 09:41:01 +0900

    objectPool의 코드들을 objectPool.h 1개로 통합

--- MyServer/MultiThreadPractice/objectPool.h
+++ MyServer/MultiThreadPractice/objectPool.h
@@ -1,14 +1,29 @@
 #pragma once
 #include <cstdint>
+#include <windows.h>
+#include <malloc.h>
+#include "pch.h"
 
 struct poolEntry : public SLIST_ENTRY {
 	uint32_t size;
 };
 
+template<typename _Ty>
 class objectPool {
+public:
+	static void* alloc() {
+		poolEntry* pTopEntry = static_cast<poolEntry*>(::InterlockedPopEntrySList(&_poolHeader));
+		if (pTopEntry == nullptr) {
+			pTopEntry = static_cast<poolEntry*>(_aligned_malloc(sizeof(poolEntry) + _typeSize, 16));
+		}
+	}
 private:
-	alignas(16) static SLIST_HEADER poolHeader;
+	alignas(16) static SLIST_HEADER _poolHeader;
+	static uint32_t _typeSize;
 };
+
+template<typename _Ty>
+uint32_t objectPool<_Ty>::_typeSize = sizeof(_Ty);
 
 template<typename _Ty, typename... Args>
 _Ty* newPool(Args&&... args) {
