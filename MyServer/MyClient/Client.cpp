06add37b8bc332390fca31cf1427539a1d9869ee
KBY\tetep <tetepiti149@gmail.com>
2024-12-31 11:24:11 +0900

    protobuf를 연습하기 위해 PB파일 추가

--- MyServer/MyClient/Client.cpp
+++ MyServer/MyClient/Client.cpp
@@ -5,4 +5,9 @@ using namespace std;
 
 int main() {
 	cout << "I'm Client" << endl;
+#ifdef PROTOBUF_NAMESPACE_OPEN
+	std::cout << "PROTOBUF_NAMESPACE_OPEN is defined." << std::endl;
+#else
+	std::cout << "PROTOBUF_NAMESPACE_OPEN is NOT defined." << std::endl;
+#endif
 }
