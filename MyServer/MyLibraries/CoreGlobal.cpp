5250e31ee5de6279c2a4d8209cc636e0532c8b9b
KBY\tetep <tetepiti149@gmail.com>
2025-01-06 16:33:28 +0900

    포인터 변수를 다룰 때, 상위 객체는 하위 객체의 weak_ptr을 들고 있도록 조정중

---
+++ MyServer/MyLibraries/CoreGlobal.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "CoreGlobal.h"
+#include "SocketUtils.h"
+
+class CoreGlobal {
+public:
+	CoreGlobal() {
+		SocketUtils::Init();
+	}
+
+	~CoreGlobal() {
+		SocketUtils::Clear();
+	}
+};
