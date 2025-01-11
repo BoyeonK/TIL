17a4a5b3cc121920b01ed8f4f960f8c6a9fcbcc3
KBY\tetep <tetepiti149@gmail.com>
2025-01-11 18:53:48 +0900

    Server에서의 로그인 요청 완료 묘사

--- MyServer/MyClient/ClientPacketHandler.cpp
+++ MyServer/MyClient/ClientPacketHandler.cpp
@@ -10,12 +10,13 @@ bool Handle_INVALID(shared_ptr<PBSession> sessionRef, unsigned char* buffer, int
 bool Handle_C_CONNECTION(shared_ptr<PBSession> sessionRef, PB::C_CONNECTION& pkt) {
 	if (pkt.isvalid()) {
 		//지금은 하드코딩해서 넣어줬지만 input으로 바꿔 줄 수도 있을 것 같다.
-		PB::S_LOGIN myAccountInfo;
+		//Unicode로 인코딩하기 귀찮으니 지금은 그냥 string을 사용해준다.
+		PB::S_LOGIN S_LOGIN_PKT;
 		string myID = "tetepiti149";
 		string myPassword = "q1w2e3r4!@";
-		myAccountInfo.set_id(myID);
-		myAccountInfo.set_password(myPassword);
-		shared_ptr<SendBuffer>loginRequestPKT = ClientPacketHandler::MakeSendBufferRef(myAccountInfo);
+		S_LOGIN_PKT.set_id(myID);
+		S_LOGIN_PKT.set_password(myPassword);
+		shared_ptr<SendBuffer>loginRequestPKT = ClientPacketHandler::MakeSendBufferRef(S_LOGIN_PKT);
 		sessionRef->Send(loginRequestPKT);
 		return true;
 	}
--- MyServer/MyServer/ServerPacketHandler.cpp
+++ MyServer/MyServer/ServerPacketHandler.cpp
@@ -4,11 +4,59 @@
 PacketHandlerFunc GPacketHandler[UINT16_MAX];
 
 bool Handle_INVALID(shared_ptr<PBSession> sessionRef, unsigned char* buffer, int32_t len) {
+#ifdef _DEBUG
+	cout << "Something goes wrong, Client sent invalid packet" << endl;
+#endif
 	return false;
 }
 
 bool Handle_S_LOGIN(shared_ptr<PBSession> sessionRef, PB::S_LOGIN& pkt) {
-	return false;
+	//유효성 검사, 실제 프로젝트라면 별도의 서버에서 진행 될 것이다.
+	string ID;
+	string password;
+	ID = pkt.id();
+	password = pkt.password();
+
+	PB::C_LOGIN C_LOGIN_PKT;
+	string token = "";
+
+	bool invalidID = false; //유효한 아이디가 아닌 경우
+	if (invalidID) {
+		C_LOGIN_PKT.set_isvalid(false);
+		C_LOGIN_PKT.set_token(token);
+		shared_ptr<SendBuffer>loginResponsePKT = ServerPacketHandler::MakeSendBufferRef(C_LOGIN_PKT);
+		sessionRef->Send(loginResponsePKT);
+		return true;
+	}
+	C_LOGIN_PKT.set_isvalid(true);
+	C_LOGIN_PKT.set_token(token);
+
+	static atomic<uint64_t> PlayerNum = 1;
+
+	//DB가 있다면, 원래 이런 정보들은 DB에 저장되어 있을 것.
+	string Cname;
+	PB::Character* knight = C_LOGIN_PKT.add_characters();
+	PB::Character* mage = C_LOGIN_PKT.add_characters();
+	PB::Character* archer = C_LOGIN_PKT.add_characters();
+	knight->set_charid(PlayerNum);
+	mage->set_charid(PlayerNum + 1);
+	archer->set_charid(PlayerNum + 2);
+
+	Cname = "타락파워전사";
+	knight->set_charname(Cname);
+	Cname = "타락파워법사";
+	mage->set_charname(Cname);
+	Cname = "타락파워궁수";
+	archer->set_charname(Cname);
+	PlayerNum.fetch_add(3);
+
+	knight->set_charclass(PB::PLAYER_TYPE_KNIGHT);
+	mage->set_charclass(PB::PLAYER_TYPE_MAGE);
+	archer->set_charclass(PB::PLAYER_TYPE_ARCHER);
+
+	shared_ptr<SendBuffer>loginResponsePKT = ServerPacketHandler::MakeSendBufferRef(C_LOGIN_PKT);
+	sessionRef->Send(loginResponsePKT);
+	return true;
 }
 
 bool Handle_S_ENTER_GAME(shared_ptr<PBSession> sessionRef, PB::S_ENTER_GAME& pkt) {
